c79c8da963d83c4c4312d95022f633a3e88123ad
Midnight <luisgabrielle1026@gmail.com>
2025-03-29 16:29:04 +0800

    add: Authentication app

--- esptfa-arima/Test_Management/urls.py
+++ esptfa-arima/Test_Management/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import upload_analysis_document
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("upload-document/", upload_analysis_document, name="upload_document"),
