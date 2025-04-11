2e7333418de48b90df8d3e3e0225c5148a2647a1
Midnight <luisgabrielle1026@gmail.com>
2025-04-11 22:59:10 +0800

    add: teacherrequiredmixin to redirect to home and include error message instead of going 403

--- esptfa_arima/esptfaARIMA/urls.py
+++ esptfa_arima/esptfaARIMA/urls.py
@@ -19,14 +19,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Test_Management.urls')),
     path('auth/', include('Authentication.urls')),
-    path('sentry-debug/', trigger_error),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
