ef196acb989660f30fccdcf3a30d09a5fbf69b9f
Midnight <luisgabrielle1026@gmail.com>
2025-04-29 11:17:05 +0800

    fix: performande indicator in indiv fa detail and replaced espta with sjlshs

--- esptfa_arima/arima_model/visualization_manager.py
+++ esptfa_arima/arima_model/visualization_manager.py
@@ -87,7 +87,7 @@ def generate_heatmap(processed_data, value_column, title=None):
         values=value_column
     )
 
-    fig, ax = plt.subplots(figsize=(12, 8))  # set fig size
+    fig, ax = plt.subplots(figsize=(15, 15))  # set fig size
 
     sns.heatmap(heatmap_data, annot=True, fmt=".1f",
                 cmap="YlGnBu", linewidths=0.5, ax=ax)
