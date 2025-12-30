4b47e94cb1e4d43d7f8cbf9c31c75b418e87186d
Dima-salang <luisgabrielle1026@gmail.com>
2025-12-30 11:55:38 +0800

    del: urls for old views for test management

--- esptfa_arima/Test_Management/urls.py
+++ esptfa_arima/Test_Management/urls.py
@@ -4,16 +4,6 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("upload-document/", upload_analysis_document, name="upload_document"),
-    path("formative-assessments/", FormativeAssessmentDashboardView.as_view(),
-         name="formative_assessment_dashboard"),
-    path("formative-assessments/<int:document_pk>/",
-         FormativeAssessmentDetailView.as_view(), name="formative_assessment_detail"),
-     path("formative-assessments/<int:document_pk>/test/<int:fa_pk>/", IndividualFADetailView.as_view(), name='individual_fa_detail'),
-     path("formative-assessments/<int:document_pk>/student/<int:student_pk>/", IndividualStudentDetailView.as_view(), name='individual_student_detail'),
-     path("formative-assessments/<int:document_pk>/delete/", delete_document, name="delete_document"),
-     path("api/formative-assessments/<int:document_pk>/delete/", delete_document_ajax, name="delete_document_ajax"),
 ]
 
 # router for test management
