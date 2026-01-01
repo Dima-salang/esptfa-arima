9f90e40420fc21d3d9dd5f407a03d7a4bf2cbf1f
Dima-salang <luisgabrielle1026@gmail.com>
2026-01-01 13:48:05 +0800

    add: getattr() in IsTeacher permission for checking if the attribute exists

--- esptfa_arima/Test_Management/permissions/permissions.py
+++ esptfa_arima/Test_Management/permissions/permissions.py
@@ -7,4 +7,4 @@ class IsTeacher(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_superuser:
             return True
-        return request.user.is_authenticated and request.user.teacher is not None
+        return request.user.is_authenticated and getattr(request.user, 'teacher', None) is not None
