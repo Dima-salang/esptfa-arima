8135730a652d1f5b2c1ad5d6b625c7f5487481d8
Dima-salang <luisgabrielle1026@gmail.com>
2026-01-02 11:09:44 +0800

    add: middle_name into serializer

--- esptfa_arima/Authentication/serializers.py
+++ esptfa_arima/Authentication/serializers.py
@@ -12,7 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password', 'acc_type', 'lrn', 'section']
+        fields = ['id', 'username', 'email', 'first_name', 'middle_name', 'last_name', 'password', 'acc_type', 'lrn', 'section']
     
     def validate_password(self, value):
         try:
